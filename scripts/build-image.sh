28025fb7641416120f7d9f20c51c989ab06fa230
Andrew Gaffney <andrew@agaffney.org>
2018-04-04 18:48:57 -0500

    script to build docker image

---
+++ scripts/build-image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BASE_IMAGE=alpine:latest
+IMAGE_NAME=ansible-sandbox
+ANSIBLE_VERSION=2.5.0
+
+set -x
+
+docker run -ti --name ${IMAGE_NAME}-build $BASE_IMAGE sh -c "apk update; apk add py3-cryptography py3-pynacl py3-bcrypt; pip3 install ansible==${ANSIBLE_VERSION}"
+docker commit ${IMAGE_NAME}-build ${IMAGE_NAME}:${ANSIBLE_VERSION}
+docker tag ${IMAGE_NAME}:${ANSIBLE_VERSION} ${IMAGE_NAME}:latest
+docker rm ${IMAGE_NAME}-build
