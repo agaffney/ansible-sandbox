73ea6a23ce0edd5db64ebbedb6dbe139e6e2b2d6
Andrew Gaffney <andrew@agaffney.org>
2018-04-05 10:36:01 -0500

    run ansible when building docker image to pre-compile .py files

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -6,7 +6,7 @@ ANSIBLE_VERSION=2.5.0
 
 set -x
 
-docker run -ti --name ${IMAGE_NAME}-build $BASE_IMAGE sh -c "apk update; apk add py3-cryptography py3-pynacl py3-bcrypt; pip3 install ansible==${ANSIBLE_VERSION}"
+docker run -ti --name ${IMAGE_NAME}-build $BASE_IMAGE sh -c "apk update; apk add py3-cryptography py3-pynacl py3-bcrypt; pip3 install ansible==${ANSIBLE_VERSION}; ansible-playbook --version"
 docker commit ${IMAGE_NAME}-build ${IMAGE_NAME}:${ANSIBLE_VERSION}
 docker tag ${IMAGE_NAME}:${ANSIBLE_VERSION} ${IMAGE_NAME}:latest
 docker rm ${IMAGE_NAME}-build
